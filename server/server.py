f80451038269a560fcf1423c81373d30e7bec361
medev21 <benmarteen07@gmail.com>
2019-04-12 20:03:14 -0400

    set up basic server

--- server/server.py
+++ server/server.py
@@ -1,4 +1,17 @@
-import flask
+from flask import Flask
+from flask_restplus import Api,Resource
+
+app = Flask(__name__)
+api = Api(app=app)
+ns = api.namespace('api', description='api operations')
+
+@ns.route("/")
+class RecipeList(Resource):
+    
+    def get(self):
+        return {'hello': 'world'}
+
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
