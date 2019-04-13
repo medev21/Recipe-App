8f3f9fa3a30207d42c5b9dfa0d8dcbec9d189f34
medev21 <benmarteen07@gmail.com>
2019-04-13 17:52:48 -0400

    add connection to db and gitignore file

--- server/server.py
+++ server/server.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from flask_restplus import Api,Resource
+from flask_sqlalchemy import SQLAlchemy
 
+# Settings
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123@localhost/recipedb'
+db = SQLAlchemy(app=app)
 api = Api(app=app)
 ns = api.namespace('api', description='api operations')
 
@@ -14,4 +19,4 @@ class RecipeList(Resource):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(port=5001)
