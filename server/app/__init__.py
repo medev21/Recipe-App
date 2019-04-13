f013372ab3328d8ec3f0ebefcf96c8e55990928d
medev21 <benmarteen07@gmail.com>
2019-04-13 18:52:41 -0400

    edit run.py

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -8,12 +8,17 @@ from flask_sqlalchemy import SQLAlchemy
 from config import app_config
 
 # db variable initializaton
-db = SQLAlchemy
+db = SQLAlchemy()
 
 def create_app(config_name):
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True) #read instance/config file
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     db.init_app(app)
+
+    #temporary route
+    @app.route('/')
+    def hello_world():
+        return 'hello, world'
 
     return app
--- server/run.py
+++ server/run.py
@@ -1,22 +1,35 @@
-from flask import Flask
-from flask_restplus import Api,Resource
-from flask_sqlalchemy import SQLAlchemy
+# run.py
 
-# Settings
-app = Flask(__name__)
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123@localhost/recipedb'
-db = SQLAlchemy(app=app)
-api = Api(app=app)
-ns = api.namespace('api', description='api operations')
+import os
 
-@ns.route("/")
-class RecipeList(Resource):
+from app import create_app
+
+config_name = os.getenv('FLASK_CONFIG')
+app = create_app(config_name)
+
+if __name__ == '__main__':
+    app.run()
+
+
+# from flask import Flask
+# from flask_restplus import Api,Resource
+# from flask_sqlalchemy import SQLAlchemy
+
+# # Settings
+# app = Flask(__name__)
+# app.config['DEBUG'] = True
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123@localhost/recipedb'
+# db = SQLAlchemy(app=app)
+# api = Api(app=app)
+# ns = api.namespace('api', description='api operations')
+
+# @ns.route("/")
+# class RecipeList(Resource):
     
-    def get(self):
-        return {'hello': 'world'}
+#     def get(self):
+#         return {'hello': 'world'}
 
 
 
-if __name__ == "__main__":
-    app.run(port=5001)
+# if __name__ == "__main__":
+#     app.run(port=5001)
